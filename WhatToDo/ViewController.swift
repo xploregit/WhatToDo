1f794d0bc3a2c8de870ddd0f14b7b466882b8a22
Hery Yang <h3ry4ng@gmail.com>
2019-04-09 17:09:27 +0700

    Initial Commit

---
+++ WhatToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToDo
+//
+//  Created by Djauhery on 9/4/19.
+//  Copyright © 2019 Djauhery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
